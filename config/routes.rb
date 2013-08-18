ef5f6aefcd0b0e8aee6aeee2cb1e89fcd26a116d
Nkhosisivile Sabelo Dlamini <village2209@yahoo.co.uk>
2013-08-18 10:33:17 +0200

    following users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 SampleApp::Application.routes.draw do
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+
+  get "users/new"
+
   get "microposts/create"
 
   get "microposts/destroy"
-
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
-
-  get "users/new"
 
   root to: 'static_pages#home'
 
